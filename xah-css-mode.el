9621ed54487e101024bef4b120577752cdbd5e5d
Xah Lee <xah@xahlee.org>
2015-06-19 19:17:29 -0700

    more keywords

--- xah-css-mode.el
+++ xah-css-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright © 2013-2015 by Xah Lee
 
 ;; Author: Xah Lee ( http://xahlee.org/ )
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Created: 18 April 2013
 ;; Keywords: languages, convenience, css, color
 ;; Homepage:  http://ergoemacs.org/emacs/xah-css-mode.html
@@ -164,7 +164,9 @@ Version 2015-04-29"
 "dd" "del" "details" "dfn" "div" "dl" "doctype" "dt" "em" "embed"
 "fieldset" "figcaption" "figure" "footer" "form" "h1" "h2" "h3" "h4"
 "h5" "h6" "head" "header" "hgroup" "hr" "html" "i" "iframe" "img"
-"input" "ins" "kbd" "keygen" "label" "legend" "li" "link" "map" "mark"
+"input" "ins" "kbd" "keygen" "label" "legend" "li" "link"
+"main"
+ "map" "mark"
 "menu" "meta" "meter" "nav" "noscript" "object" "ol" "optgroup"
 "option" "output" "p" "param" "pre" "progress" "q" "rp" "rt" "ruby"
 "s" "samp" "script" "section" "select" "small" "source" "span"
